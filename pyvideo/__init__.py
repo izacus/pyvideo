f0e5b1036abec220df44b1b29c6e43900b6311e2
Jernej Virag <jernej.virag@gmail.com>
2012-05-07 11:04:03 +0200

    Added ability to skip decoding of video

--- pyvideo/__init__.py
+++ pyvideo/__init__.py
@@ -5,6 +5,6 @@ import avbin
 
 __author__ = 'Jernej Virag'
 
-def load(filename):
-    source = avbin.AVbinSource(filename)
+def load(filename, skip_video=False):
+    source = avbin.AVbinSource(filename, skip_video=skip_video)
     return source
