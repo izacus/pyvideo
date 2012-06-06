527b848101c46f6f40d857ffdd91b135d4aaaa9f
Jernej Virag <jernej.virag@gmail.com>
2012-06-06 15:35:31 +0200

    Added metadata tests

--- tests.py
+++ tests.py
@@ -2,12 +2,23 @@ import unittest
 from unittest.case import TestCase
 import pyvideo
 
-class DecodingPerformanceTests(TestCase):
+class DecodingComplianceTests(TestCase):
     def setUp(self):
         pass
+    
+    def testMetaData(self):
+        source = pyvideo.load("test_media/test_video.mp4")
+        self.assertAlmostEqual(source.duration, 10.041667)
+        video_format = source.video_format
+        self.assertEqual(video_format.height, 480)
+        self.assertEqual(video_format.width, 854)
+        audio_format = source.audio_format
+        self.assertEqual(audio_format.channels, 2)
+        self.assertEqual(audio_format.sample_size, 16)
+        self.assertEqual(audio_format.sample_rate, 48000)
 
     def testFullDecode(self):
-        source = pyvideo.load("test_media/test_video.mp4")
+        source = pyvideo.load("test_media/victoria.webm")
         self.assertIsNotNone(source.audio_format)
         self.assertIsNotNone(source.video_format)
 
